0ff6785002fb5ad4ecff0937f34939c2ef5b672b
tonyshu168 <493397276@qq.com>
2021-11-05 10:48:36 +0800

    生成小球的动画

--- src/ts/countdown.ts
+++ src/ts/countdown.ts
@@ -9,8 +9,19 @@ import digit from './digit';
         canvas = document.getElementById('canvas') as HTMLCanvasElement,
         ctx = canvas.getContext('2d')!;
 
-  const endTime = new Date('2021-10-31 23:00:00');
+  const endTime = new Date('2021-11-06 23:00:00');
   let curShowTimeSeconds = 0;
+
+  type BallType = {
+    x: number,
+    y: number,
+    g: number,
+    vx: number,
+    vy: number,
+    color: string
+  };
+  const balls: Array<BallType> = [],
+        colors = ['#33B5E5','#0099CC','#AA66CC','#9933CC','#99CC00','#669900','#FFBB33','#FF8800','#FF4444','#CC0000'];
 
   canvas.width = WINDOW_WIDTH, canvas.height = WINDOW_HEIGHT;
 
@@ -32,8 +43,32 @@ import digit from './digit';
         curMinutes = parseInt( String((curShowTimeSeconds - curHours * 3600)/60) ),
         curSeconds = curShowTimeSeconds % 60;
     if ( nextSeconds !== curSeconds ) {
+      // 设置变动的小球
+      if ( parseInt( String(curHours/10) ) != parseInt( String(nextHours/10) ) ) {
+        addBalls( MARGIN_LEFT + 0, MARGIN_TOP, parseInt(String(curHours/10)) );
+      }
+      if ( parseInt( String(curHours%10) ) != parseInt( String(nextHours%10) ) ) {
+        addBalls( MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt( String(curHours/10) ) );
+      }
+
+      if( parseInt( String(curMinutes/10) ) != parseInt( String(nextMinutes/10) ) ){
+        addBalls( MARGIN_LEFT + 39 * (RADIUS+1) , MARGIN_TOP , parseInt( String(curMinutes/10) ) );
+      }
+      if( parseInt( String(curMinutes%10) ) != parseInt( String(nextMinutes%10) ) ){
+        addBalls( MARGIN_LEFT + 54 * (RADIUS+1) , MARGIN_TOP , parseInt( String(curMinutes%10) ) );
+      }
+
+      if( parseInt( String(curSeconds/10) ) != parseInt( String(nextSeconds/10) ) ){
+        addBalls( MARGIN_LEFT + 78 * (RADIUS+1) , MARGIN_TOP , parseInt( String(curSeconds/10) ) );
+      }
+      if( parseInt( String(curSeconds%10) ) != parseInt( String(nextSeconds%10) ) ){
+        addBalls( MARGIN_LEFT + 93 * (RADIUS+1) , MARGIN_TOP , parseInt( String(nextSeconds%10) ) );
+      }
+
       curShowTimeSeconds = nextShowTimeSeconds;
     }
+
+    updateBalls();
   }
 
   function getCurrentShowTimeSeconds(): number {
@@ -64,6 +99,17 @@ import digit from './digit';
     // 秒
     renderDigit( MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(String(seconds / 10)), ctx);
     renderDigit( MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(String(seconds % 10)), ctx);
+    
+    // 生成动画小球
+    for ( const ball of balls ) {
+      ctx.fillStyle = ball.color;
+
+      ctx.beginPath();
+      ctx.arc( ball.x, ball.y, RADIUS, 0, 2 * Math.PI, true);
+      ctx.closePath();
+
+      ctx.fill();
+    }
   }
 
   function renderDigit( x: number, y: number, num: number, ctx: CanvasRenderingContext2D ) {
@@ -77,6 +123,40 @@ import digit from './digit';
           ctx.closePath();
 
           ctx.fill();
+        }
+      }
+    }
+  }
+
+  function updateBalls( ) {
+    for ( const obj of balls ) {
+      obj.x += obj.vx;
+      obj.y += obj.vy;
+      obj.vy += obj.g;
+
+      // 边界判断
+      if ( obj.y >= WINDOW_HEIGHT - RADIUS ) {
+        obj.y = WINDOW_HEIGHT - RADIUS;
+        obj.vy =- obj.vy * 0.75;
+      }
+    }
+  }
+
+  // Add ball
+  function addBalls( x: number, y: number, num: number) {
+    for ( let i = 0, len = digit[num].length; i < len; i++ ) {
+      for ( let j = 0, length = digit[num][i].length; j < length; j++ ) {
+        if ( digit[num][i][j] === 1 ) {
+          const aBall: BallType = {
+            x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+            y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+            g: 1.5 + Math.random(),
+            vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,            // -1多少次方, [-1, 1], 偶数次方为1, 奇数次方为-1
+            vy: -5,
+            color: colors[ Math.floor(Math.random() * colors.length) ]
+          };
+
+          balls.push( aBall );
         }
       }
     }
